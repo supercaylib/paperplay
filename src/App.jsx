7c59b453ba5e76aea27213e82119a1c4c5dee2dd
supercaylib <calebraagasp.com>
2026-02-10 10:18:54 +0800

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -113,14 +113,6 @@ function LandingPage() {
         
         <div className="button-grid">
           
-          {/* PHYSICAL REQUEST */}
-          <Link to="/request">
-            <button className="action-btn btn-outline">
-              <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M12 19l7-7 3 3-7 7-3-3z"></path><path d="M18 13l-1.5-7.5L2 2l3.5 14.5L13 18l5-5z"></path><path d="M2 2l7.586 7.586"></path><circle cx="11" cy="11" r="2"></circle></svg>
-              <span>Request Physical Letter</span>
-            </button>
-          </Link>
-
           {/* DIGITAL LETTER */}
           <Link to="/create">
             <button className="action-btn btn-solid">
@@ -129,6 +121,13 @@ function LandingPage() {
             </button>
           </Link>
 
+          {/* PHYSICAL REQUEST */}
+          <Link to="/request">
+            <button className="action-btn btn-outline">
+              <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M12 19l7-7 3 3-7 7-3-3z"></path><path d="M18 13l-1.5-7.5L2 2l3.5 14.5L13 18l5-5z"></path><path d="M2 2l7.586 7.586"></path><circle cx="11" cy="11" r="2"></circle></svg>
+              <span>Request Physical Letter</span>
+            </button>
+          </Link>
         </div>
       </div>
 
