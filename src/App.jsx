25b5f953a88b56b4494b4dbf282bd1c79c93a2e7
supercaylib <calebraagasp.com>
2026-02-10 10:22:23 +0800

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -75,7 +75,7 @@ function LandingPage() {
       <div className="center-text" style={{marginBottom: '30px'}}>
         <img src="/logo.png" alt="Logo" className="logo-img" />
         <h1>PaperPlay</h1>
-        <p className="subtitle">Greatest Letter Maker</p>
+        <p className="subtitle">The Greatest Letter Website</p>
       </div>
 
       {/* SMART TRACKING SECTION */}
@@ -132,7 +132,7 @@ function LandingPage() {
       </div>
 
       <div className="footer">
-        <p>Caleb R. Pule</p>
+        <p>Ragx </p>
       </div>
 
     </div>
